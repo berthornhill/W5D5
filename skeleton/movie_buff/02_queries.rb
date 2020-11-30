b1d5c6ce27c5c94ac2db95935f2f3f3c8fea3694
russelkamal <russelhkamal@gmail.com>
2020-11-30 16:46:50 -0500

    pt2

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -33,11 +33,18 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+  Movie.select(:id, :title, "actors.name").joins(:actors).where("castings.ord = 1 AND director_id = actor_id")
 
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor.select(:id, :name, "COUNT(*) AS roles")
+  .joins(:castings)
+  .where("castings.ord > 1")
+  .group("actors.id")
+  .order("roles DESC")
+  .limit(2)
+  
 end
