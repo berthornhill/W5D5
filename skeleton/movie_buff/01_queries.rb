c232d006e27e8bcbfc49180869468b2f91dd45b1
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-11-30 15:33:42 -0500

    Completed 01, began 02

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -68,7 +68,7 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
-  Movie.select(:id, :title).joins(:director).where("name IN (?)", them)
+  Movie.select(:id, :title).joins(:director).where("actors.name IN (?)", them)
 
 end
 
@@ -83,5 +83,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie.where( "yr < 1940").pluck(:title)
 
 end
