fcca586444ae312b42cd338bb68920f38411f825
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-11-30 17:14:02 -0500

    Began 03

--- skeleton/movie_buff/03_queries.rb
+++ skeleton/movie_buff/03_queries.rb
@@ -1,12 +1,19 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+    # Movie.select(:id, :title)
+    #   .joins(:actors)
+    #   .where("name IN ?", those_actors)
 
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  Movie.select("yr/10")
+    .group("yr/10")
+    .order("AVG(score) DESC")
+    .limit(1)
+    .pluck("yr/10 * 10").first
 end
 
 def costars(name)
